ba062e5fd3261d497d44a855815198fa2514b84b
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-03-07 16:10:23 +0300

    finished backend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from '../models/product.model.js'
+import mongoose from "mongoose"
 
 export const getProducts = async (req, res)=>{
     try {
@@ -44,9 +45,9 @@ export const deleteProduct = async (req, res) => {
     const { id } = req.params;
     try {
         await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: false, message: "Product deleted"})
+        res.status(200).json({ success: true, message: "Product deleted"})
     } catch(error) {
         console.log("error in deleting product:", error.message);
-        res.status(404).json({ success: false, message: "Product not found"})
+        res.status(500).json({ success: false, message: "Server Error"})
     }
 }
