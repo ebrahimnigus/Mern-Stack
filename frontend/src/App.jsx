1f7c48a6d98831acf2ce7b64d5e477241a0679d4
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-03-09 21:52:55 +0300

    added navbar, pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
-import { Box, Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import Navbar from "./component/Navbar"
+
 
 function App() {
   return (
     <Box minH={"100vh"}>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
