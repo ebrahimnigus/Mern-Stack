3b0a0593602a032aeed2eff044367e01539a29fe
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-04-01 22:09:16 +0300

    add products container

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,19 @@
+import { Container, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Container max='container-xl' py={12}>
+      <VStack spacing={8}>
+        <Text fontSize={"30"}
+        fontWeight={"bold"}
+        bgGradient={"linear(to-r, cyan.400, blue.500)"}
+        bgClip={"text"}
+        textAlign>
+          Current Products 💉
+        </Text>
+      </VStack>
+    </Container>
   )
 }
 
